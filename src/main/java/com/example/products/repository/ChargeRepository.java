7d601423a6cf10fc42f76a03ae4f4c25caf9c2b7
02tanvi <148671050+02tanvi@users.noreply.github.com>
2025-10-11 17:52:42 +0400

    Delete src directory

--- src/main/java/com/example/products/repository/ChargeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.products.repository;
-
-import com.example.products.entity.Charge;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ChargeRepository extends JpaRepository<Charge, Long> {
-    List<Charge> findByProductId(Long productId);
-}
