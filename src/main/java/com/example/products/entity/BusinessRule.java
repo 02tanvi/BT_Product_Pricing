af3c79e52c3ad517eb9a77a1f9364e61b1e77f48
02tanvi <148671050+02tanvi@users.noreply.github.com>
2025-10-14 18:43:04 +0400

    Update BusinessRule.java

--- src/main/java/com/example/products/entity/BusinessRule.java
+++ src/main/java/com/example/products/entity/BusinessRule.java
@@ -65,5 +65,116 @@ public class BusinessRule {
         updatedAt = LocalDateTime.now();
     }
 
-    // Getters & Setters (same as before)
+	public Long getRuleId() {
+		return ruleId;
+	}
+
+	public void setRuleId(Long ruleId) {
+		this.ruleId = ruleId;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public Integer getMinTerm() {
+		return minTerm;
+	}
+
+	public void setMinTerm(Integer minTerm) {
+		this.minTerm = minTerm;
+	}
+
+	public Integer getMaxTerm() {
+		return maxTerm;
+	}
+
+	public void setMaxTerm(Integer maxTerm) {
+		this.maxTerm = maxTerm;
+	}
+
+	public BigDecimal getMinAmount() {
+		return minAmount;
+	}
+
+	public void setMinAmount(BigDecimal minAmount) {
+		this.minAmount = minAmount;
+	}
+
+	public BigDecimal getMaxAmount() {
+		return maxAmount;
+	}
+
+	public void setMaxAmount(BigDecimal maxAmount) {
+		this.maxAmount = maxAmount;
+	}
+
+	public BigDecimal getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(BigDecimal interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	public String getCompoundingFrequency() {
+		return compoundingFrequency;
+	}
+
+	public void setCompoundingFrequency(String compoundingFrequency) {
+		this.compoundingFrequency = compoundingFrequency;
+	}
+
+	public Boolean getPrematureWithdrawalAllowed() {
+		return prematureWithdrawalAllowed;
+	}
+
+	public void setPrematureWithdrawalAllowed(Boolean prematureWithdrawalAllowed) {
+		this.prematureWithdrawalAllowed = prematureWithdrawalAllowed;
+	}
+
+	public BigDecimal getPrematurePenaltyRate() {
+		return prematurePenaltyRate;
+	}
+
+	public void setPrematurePenaltyRate(BigDecimal prematurePenaltyRate) {
+		this.prematurePenaltyRate = prematurePenaltyRate;
+	}
+
+	public Boolean getAutoRenewal() {
+		return autoRenewal;
+	}
+
+	public void setAutoRenewal(Boolean autoRenewal) {
+		this.autoRenewal = autoRenewal;
+	}
+
+	public BigDecimal getMinBalanceRequired() {
+		return minBalanceRequired;
+	}
+
+	public void setMinBalanceRequired(BigDecimal minBalanceRequired) {
+		this.minBalanceRequired = minBalanceRequired;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+    
 }
