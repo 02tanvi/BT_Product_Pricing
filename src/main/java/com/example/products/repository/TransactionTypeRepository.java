3f7214af64a9ec27cde882330782aef5c7ee469f
02tanvi <148671050+02tanvi@users.noreply.github.com>
2025-10-15 16:23:27 +0400

    Delete src directory

--- src/main/java/com/example/products/repository/TransactionTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.products.repository;
-
-import com.example.products.entity.TransactionType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TransactionTypeRepository extends JpaRepository<TransactionType, Long> {
-    List<TransactionType> findByProductId(Long productId);
-}
